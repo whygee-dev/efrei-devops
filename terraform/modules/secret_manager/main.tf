412508acc2bd9dc33598c0701173bc3783713c5d
Yahya Jirari <yahya.jirari@beeldi.com>
2023-05-19 19:48:58 +0200

    Add cloud build service account to secret manager policy

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -10,9 +10,23 @@ resource "google_secret_manager_secret" "secret" {
   }
 }
 
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/secretmanager.secretAccessor"
+    members = [
+      "serviceAccount:292481049315@cloudbuild.gserviceaccount.com",
+    ]
+  }
+}
+
 resource "google_secret_manager_secret_version" "version" {
   secret        = google_secret_manager_secret.secret.id
   secret_data   = var.secret_value
   depends_on    = [google_secret_manager_secret.secret]
 }
 
+resource "google_secret_manager_secret_iam_policy" "policy" {
+  project      = google_secret_manager_secret.secret.project
+  secret_id    = google_secret_manager_secret.secret.secret_id
+  policy_data  = data.google_iam_policy.admin.policy_data
+}
